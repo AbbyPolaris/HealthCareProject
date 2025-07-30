b4a39e9c5195ebfd004c4d0547edb2468d79997e
Abby <northern-crown@outlook.com>
2025-07-30 19:50:15 +0330

    added variables and parameters.

--- model.py
+++ model.py
@@ -2,3 +2,35 @@ from pyomo.environ import *
 infinity = float('inf')
 model = AbstractModel(name='HealthCare Project')
 
+
+#sets here
+model.Nodes = Set()
+model.Hospitals = Set()
+model.BDCs = Set()
+model.OpenBDCs = Set()
+
+#model parameters here
+model.M = 999999999
+model.epsilon = 1e-9
+
+model.g = Param(model.BDCs, within= NonNegativeReals)
+model.p = Param(model.BDCs, within = NonNegativeIntegers)
+model.D = Param()
+model.h = Param(model.Hospitals, within= NonNegativeIntegers)
+model.e_b = Param(model.Hospitals, [0,1],within= NonNegativeReals)
+model.tn_n = Param(model.Nodes, model.Nodes,within= NonNegativeReals)
+model.u = Param()
+model.Q = Param()
+model.s = Param()
+
+
+#variables here
+model.xn_n = Var(model.Nodes, Model.Nodes, within = Binary)
+model.y = Var(model.BDCs, within= Binary)
+model.zn_n = Var(model.Nodes, model.Nodes, within= NonNegativeIntegers)
+model.U = Var(model.Hospitals, within= NonNegativeReals)
+model.v = Var(model.Hospitals, within= NonNegativeReals)
+model.GG = Var(model.Hospitals, within= NonNegativeReals)
+model.EE = Var(model.Hospitals, within= NonNegativeReals)
+
+#constraints here
